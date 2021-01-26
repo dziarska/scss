10ff812dfdb2c4440812576d4440e4566e5b3247
DZIARSKA <ZDZIARSKA.KATARZYNA@GMAIL.COM>
2021-01-26 22:07:00 +0100

    3.8 is next

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
 module.exports = {
     entry: "./src/index.js",
     output: {
@@ -14,7 +15,8 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
         template: "./src/index.html"
-        })
+        }),
+        new CleanWebpackPlugin()
         ],
     module: {
         rules: [{
